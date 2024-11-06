b5ec0479d589f00041d3a1e442becaac1259a1cf
susgleik <angelhernades26@gmail.com>
2024-11-06 02:37:38 -0500

    correcion para que en desarrollo funcione

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-      },
-    ];
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
+  /* config options here */
 }
-
-module.exports = nextConfig;
+ 
+module.exports = nextConfig
