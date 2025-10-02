6baad8dd5a8bb37ed3186b95e3c786232bcddf91
susgleik <angelhernades26@gmail.com>
2025-10-02 01:42:29 -0500

    modificacion de layout main para support de dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from './providers/ThemeProvider'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,9 +18,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="es">
+    <html lang="es" suppressHydrationWarning>
       <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
         {children}
+        </ThemeProvider>
       </body>
     </html>
   )
